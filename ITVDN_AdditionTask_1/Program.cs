674fb2dd847e2e00ad3572e4ac1ed3560aa73625
karmazin.i.m <karmazin.i.m@gmail.com>
2019-09-14 11:31:51 +0300

    Addition Task

---
+++ ITVDN_AdditionTask_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace ITVDN_AdditionTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("TelephoneBook.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
+            {
+                XmlWriter xml = XmlWriter.Create(fs);
+
+                xml.WriteStartDocument();
+                xml.WriteStartElement("MyContacts");
+                xml.WriteStartElement("Contact");
+                xml.WriteAttributeString("Name","Illya");
+                xml.
+                xml.WriteEndElement();
+                xml.WriteEndElement();
+                xml.WriteEndDocument();
+
+                xml.Close();
+            }
+
+            Console.WriteLine("Program is ended...");
+            Console.ReadKey();
+        }
+    }
+}
