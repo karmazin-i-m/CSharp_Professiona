e94479bb44da5daf6d7322a8e61e03f6e2424a95
karmazin.i.m <karmazin.i.m@gmail.com>
2019-09-27 16:59:08 +0300

    Work with OpenFileMenu

--- ITVDN_AdditionTask_1/Program.cs
+++ ITVDN_AdditionTask_1/Program.cs
@@ -9,11 +9,15 @@ namespace ITVDN_AdditionTask_1
 {
     class Program
     {
-        private const int TextBufferHeight = 40;
-        private const int TextBufferWidth = 120;
+        private const int DefaultTextBufferHeight = 40;
+        private const int DefaultTextBufferWidth = 120;
+
+        private static String path;
 
         static void Main(string[] args)
         {
+            ProgrammSettings();
+
             View(ViewState.Start);
             Menu();
 
@@ -22,9 +26,9 @@ namespace ITVDN_AdditionTask_1
 
         private static void View(ViewState state)
         {
-            Console.SetWindowSize(TextBufferWidth, TextBufferHeight);
+            Console.Clear();
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb;
 
             Console.Write("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
             Console.Write("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX             REFLECTOR TEXT             XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
@@ -37,28 +41,90 @@ namespace ITVDN_AdditionTask_1
             {
                 case ViewState.Start:
 
+                    sb = new StringBuilder();
+
                     for (int i = 0; i < 33; i++)
                     {
-                        sb.Append("X                                                                                                                      X");
+                        sb.Append('X');
+                        sb.Append(new String(' ', DefaultTextBufferWidth - 2));
+                        sb.Append('X');
                     }
 
-                    break;
+                    sb.Append('X', 119);
+
+                    Console.Write(sb.ToString());
+
+                    Console.CursorLeft = 1;
+                    Console.CursorTop = 38;
+                    return;
+
                 case ViewState.Open:
 
-                    for (int i = 0; i < 33; i++)
+                    sb = new StringBuilder();
+
+                    String str = " Введите полный путь к файлу: ";
+                    sb.Append('X');
+                    sb.Append(str);
+                    sb.Append(' ', DefaultTextBufferWidth - 2 - str.Length);
+                    sb.Append('X');
+
+                    for (int i = 0; i < 30; i++)
                     {
-                        sb.Append("X                                                                                                                      X");
+                        sb.Append('X');
+                        sb.Append(new String(' ', DefaultTextBufferWidth - 2));
+                        sb.Append('X');
+                    }
+                    sb.Append('X', 119);
+                    Console.Write(sb.ToString());
+
+                    Console.CursorLeft = DefaultTextBufferWidth - (DefaultTextBufferWidth - str.Length - 1);
+                    Console.CursorTop = 6;
+                    path = Console.ReadLine();
+                    View(ViewState.ShowAssembly);
+                    return;
+                case ViewState.ShowAssembly:
+
+                    Assembly assembly;
+
+                    sb = new StringBuilder();
+
+                    try
+                    {
+                        assembly = Assembly.Load(path);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return;
                     }
 
-                    break;
+                    Type[] types = assembly.GetTypes();
 
+                    if (types.Length > 0)
+                    {
+                        int count = 0;
+                        foreach (Type item in types)
+                        {
+                            str = $" {count}. ";
+                            sb.Append('X');
+                            sb.Append(str + item.FullName);
+                            sb.Append(' ', DefaultTextBufferWidth - str.Length - item.FullName.Length - 2);
+                            sb.Append('X');
+                            count++;
+                        }
+
+                        for (int i = 0; i < 33 - types.Length; i++)
+                        {
+                            sb.Append('X');
+                            sb.Append(new String(' ', DefaultTextBufferWidth - 2));
+                            sb.Append('X');
+                        }
+                        sb.Append('X', 119);
+                        Console.Write(sb.ToString());
+                    }
+
+                    return;
             }
-
-
-            sb.Append("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.Write(sb.ToString());
-            Console.CursorLeft = 1;
-            Console.CursorTop = 38;
         }
 
         private static void Menu()
@@ -70,7 +136,7 @@ namespace ITVDN_AdditionTask_1
                 switch (key)
                 {
                     case ConsoleKey.D1:
-
+                        View(ViewState.Open);
                         break;
                     case ConsoleKey.D2:
 
@@ -81,9 +147,10 @@ namespace ITVDN_AdditionTask_1
             }
         }
 
-        private static void OpenFile()
+        private static void ProgrammSettings()
         {
-
+            Console.SetBufferSize(DefaultTextBufferWidth, DefaultTextBufferHeight);
+            Console.SetWindowSize(DefaultTextBufferWidth, DefaultTextBufferHeight);
         }
 
 
@@ -93,6 +160,7 @@ namespace ITVDN_AdditionTask_1
     {
         Start,
         Open,
+        ShowAssembly,
 
     }
 }
