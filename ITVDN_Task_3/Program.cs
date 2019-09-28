1521cd057f9b8c5f439595038de36ade087ebe26
karmazin.i.m <karmazin.i.m@gmail.com>
2019-09-28 13:02:26 +0300

    task 3 is completed

--- ITVDN_Task_3/Program.cs
+++ ITVDN_Task_3/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace ITVDN_Task_3
 {
     class Program
     {
+        static Assembly assembly;
         static void Main(string[] args)
         {
+            try
+            {
+                assembly = Assembly.Load("ITVDN_Task_2");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникла ошибка: " + e.Message);
+                Console.ResetColor();
+            }
+
+            try
+            {
+                Type converter = assembly.GetType("ITVDN_Task_2.Converter");
+                MethodInfo celsiusToFaringate = converter.GetMethod("CelsiusToFaringate");
+                Console.WriteLine(celsiusToFaringate.Invoke(converter, new object[] { 0 }));
+            }
+            catch (ArgumentException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникла ошибка: " + e.Message);
+                Console.ResetColor();
+            }
+
+            Console.ReadKey();
         }
     }
 }
