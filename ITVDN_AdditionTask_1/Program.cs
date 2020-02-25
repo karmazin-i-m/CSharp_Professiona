0d4dcd03ea6b85f813a9f37d3da325f6d5778253
karmazin.i.m <karmazin.i.m@gmail.com>
2020-02-25 22:33:20 +0200

    Add project files.

---
+++ ITVDN_AdditionTask_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ITVDN_AdditionTask_1
+{
+    class Program
+    {
+        static Int32 a = 0;
+        private static readonly Object SyncRoot = new Object();
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Count);
+            Thread thread2 = new Thread(Count);
+            Thread thread3 = new Thread(Count);
+
+            thread1.Name = "Поток 1";
+            thread2.Name = "Поток 2";
+            thread3.Name = "Поток 3";
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            Console.ReadKey();
+        }
+
+        public static void Count()
+        {
+            lock (SyncRoot)
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Поток {Thread.CurrentThread.Name} говорит {++a}");
+                }
+        }
+    }
+}
---
+++ ITVDN_Task_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace ITVDN_Task_2
+{
+    class Program
+    {
+        private static readonly Object SyncRoot = new Object();
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() =>
+            {
+                String pathRead = Directory.GetCurrentDirectory() + "\\FIle1.txt";
+                String pathWrite = Directory.GetCurrentDirectory() + "\\FIle3.txt";
+
+                WriteFile(pathWrite, ReadFile(pathRead));
+            });
+            Thread thread2 = new Thread(() =>
+            {
+                String pathRead = Directory.GetCurrentDirectory() + "\\FIle2.txt";
+                String pathWrite = Directory.GetCurrentDirectory() + "\\FIle3.txt";
+
+                WriteFile(pathWrite, ReadFile(pathRead));
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+
+        static String ReadFile(String path)
+        {
+            using (Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = File.OpenText(path))
+                return reader.ReadToEnd();
+
+        }
+
+        static void WriteFile(String path, String text)
+        {
+            lock (SyncRoot)
+                using (Stream stream = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                    using (StreamWriter writer = new StreamWriter(stream))
+                        writer.Write(text);
+        }
+    }
+}
