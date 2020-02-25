2c761412b2b98521e5fafb5191895c4c707a13cb
karmazin.i.m <karmazin.i.m@gmail.com>
2020-02-25 22:28:00 +0200

    Completed homework

--- ITVDN_Task_3/Program.cs
+++ ITVDN_Task_3/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ITVDN_Task_3
 {
     class Program
     {
+        private const String _name = "MY PROGRAMM";
+        private static Mutex _mutex;
         static void Main(string[] args)
         {
+            if (Mutex.TryOpenExisting(_name, out _mutex))
+                Process.GetCurrentProcess().Kill();
+            else
+                _mutex = new Mutex(true, _name);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+
+            _mutex.Dispose();
         }
     }
 }
